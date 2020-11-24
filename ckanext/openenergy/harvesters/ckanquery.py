6353ead4ec89131ff015f6feb80751390f02a871
flowirtz <6052785+flowirtz@users.noreply.github.com>
2020-11-24 14:31:33 +0000

    Fix fq vs q param

--- ckanext/openenergy/harvesters/ckanquery.py
+++ ckanext/openenergy/harvesters/ckanquery.py
@@ -1,10 +1,11 @@
 import datetime
 import logging
 
-from ckanext.harvest.harvesters.ckanharvester import CKANHarvester, SearchError
+from ckanext.harvest.harvesters.ckanharvester import CKANHarvester, SearchError, ContentFetchError
 from ckanext.harvest.model import HarvestObject
 from ckan.plugins import toolkit
 from ckan.lib.helpers import json
+from six.moves.urllib.parse import urlencode
 
 
 log = logging.getLogger(__name__)
@@ -30,13 +31,10 @@ class CKANQueryHarvester(CKANHarvester):
         # Get source URL
         remote_ckan_base_url = harvest_job.source.url.rstrip('/')
 
-        fq_terms = []
-        
         # FILTER BY SEARCH TERM
         search_query_string = self.config.get('search_query', None)
-        if search_query_string:
-            fq_terms.append('q=%s' % search_query_string)
         
+        fq_terms = []
         # Filter in/out datasets from particular organizations
         org_filter_include = self.config.get('organizations_filter_include', [])
         org_filter_exclude = self.config.get('organizations_filter_exclude', [])
@@ -81,7 +79,9 @@ class CKANQueryHarvester(CKANHarvester):
             try:
                 pkg_dicts = self._search_for_datasets(
                     remote_ckan_base_url,
-                    fq_terms + [fq_since_last_time])
+                    fq_terms + [fq_since_last_time],
+                    search_query_string
+                )
             except SearchError as e:
                 log.info('Searching for datasets changed since last time '
                          'gave an error: %s', e)
@@ -98,7 +98,7 @@ class CKANQueryHarvester(CKANHarvester):
             # Request all remote packages
             try:
                 pkg_dicts = self._search_for_datasets(remote_ckan_base_url,
-                                                      fq_terms)
+                                                      fq_terms, search_query_string)
             except SearchError as e:
                 log.info('Searching for all datasets gave an error: %s', e)
                 self._save_gather_error(
@@ -136,3 +136,80 @@ class CKANQueryHarvester(CKANHarvester):
             return object_ids
         except Exception as e:
             self._save_gather_error('%r' % e.message, harvest_job)
+
+    def _search_for_datasets(self, remote_ckan_base_url, fq_terms=None, q_string=None):
+        '''Does a dataset search on a remote CKAN and returns the results.
+        Deals with paging to return all the results, not just the first page.
+        '''
+        base_search_url = remote_ckan_base_url + self._get_search_api_offset()
+        params = {'rows': '100', 'start': '0'}
+        # There is the worry that datasets will be changed whilst we are paging
+        # through them.
+        # * In SOLR 4.7 there is a cursor, but not using that yet
+        #   because few CKANs are running that version yet.
+        # * However we sort, then new names added or removed before the current
+        #   page would cause existing names on the next page to be missed or
+        #   double counted.
+        # * Another approach might be to sort by metadata_modified and always
+        #   ask for changes since (and including) the date of the last item of
+        #   the day before. However if the entire page is of the exact same
+        #   time, then you end up in an infinite loop asking for the same page.
+        # * We choose a balanced approach of sorting by ID, which means
+        #   datasets are only missed if some are removed, which is far less
+        #   likely than any being added. If some are missed then it is assumed
+        #   they will harvested the next time anyway. When datasets are added,
+        #   we are at risk of seeing datasets twice in the paging, so we detect
+        #   and remove any duplicates.
+        params['sort'] = 'id asc'
+        if fq_terms:
+            params['fq'] = ' '.join(fq_terms)
+        
+        if q_string:
+            params['q'] = q_string
+
+        pkg_dicts = []
+        pkg_ids = set()
+        previous_content = None
+        while True:
+            url = base_search_url + '?' + urlencode(params)
+            log.debug('Searching for CKAN datasets: %s', url)
+            try:
+                content = self._get_content(url)
+            except ContentFetchError as e:
+                raise SearchError(
+                    'Error sending request to search remote '
+                    'CKAN instance %s using URL %r. Error: %s' %
+                    (remote_ckan_base_url, url, e))
+
+            if previous_content and content == previous_content:
+                raise SearchError('The paging doesn\'t seem to work. URL: %s' %
+                                  url)
+            try:
+                response_dict = json.loads(content)
+            except ValueError:
+                raise SearchError('Response from remote CKAN was not JSON: %r'
+                                  % content)
+            try:
+                pkg_dicts_page = response_dict.get('result', {}).get('results',
+                                                                     [])
+            except ValueError:
+                raise SearchError('Response JSON did not contain '
+                                  'result/results: %r' % response_dict)
+
+            # Weed out any datasets found on previous pages (should datasets be
+            # changing while we page)
+            ids_in_page = set(p['id'] for p in pkg_dicts_page)
+            duplicate_ids = ids_in_page & pkg_ids
+            if duplicate_ids:
+                pkg_dicts_page = [p for p in pkg_dicts_page
+                                  if p['id'] not in duplicate_ids]
+            pkg_ids |= ids_in_page
+
+            pkg_dicts.extend(pkg_dicts_page)
+
+            if len(pkg_dicts_page) == 0:
+                break
+
+            params['start'] = str(int(params['start']) + int(params['rows']))
+
+        return pkg_dicts
